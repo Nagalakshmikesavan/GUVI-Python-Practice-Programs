52568ed664f029978900e732a05c54bf6d188d41
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-06-27 13:48:33 +0530

    Update last.py

--- last.py
+++ last.py
@@ -2,14 +2,17 @@ ab=input()
 sp1=ab.split(" ")
 a=sp1[0]
 b=sp1[1]
-print(a,b)
 mn=input()
 sp2=mn.split(" ")
 m=sp2[0]
 n=sp2[1]
-print(m,n)
 xy=input()
 sp3=xy.split(" ")
 x=sp3[0]
 y=sp3[1]
-print(x,y)
+d=(int(n)-int(b))*(int(x)-int(m))
+f=(int(y)-int(n))*(int(m)-int(a))
+if(d==f):
+	print("yes")
+else:
+	print("no")
