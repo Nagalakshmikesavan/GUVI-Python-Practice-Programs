f56ff85768a561be0e63f8e37cdfc35f5989d74f
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-06-27 11:23:26 +0530

    Create last.py

---
+++ last.py
@@ -0,0 +1,15 @@
+ab=input()
+sp1=ab.split(" ")
+a=sp1[0]
+b=sp1[1]
+print(a,b)
+mn=input()
+sp2=mn.split(" ")
+m=sp2[0]
+n=sp2[1]
+print(m,n)
+xy=input()
+sp3=xy.split(" ")
+x=sp3[0]
+y=sp3[1]
+print(x,y)
