b8d925fae3da5bd22ff1badea1a078d82f22e116
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-07-31 23:28:04 +1000

    Update hunt7.py

--- hunt7.py
+++ hunt7.py
@@ -1,10 +1,10 @@
 n=int(input())
 l=[]
 m=input().split()
-for i in range(0,n,2):
-	if int(m[i])%2!=0:
+for i in range(n):
+	if i%2==0 and  int(m[i])%2!=0:
 		l.append(m[i])
-for i in range(1,n,2):
-	if int(m[i])%2==0:
+	elif i%2!=0 and int(m[i])%2==0:
 		l.append(m[i])
+	
 print(*(l))
