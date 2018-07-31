3ab1281a25a5cedbda5b8ec10cff207f91d848b9
Nagalakshmikesavan <34570264+Nagalakshmikesavan@users.noreply.github.com>
2018-08-01 00:23:50 +1000

    Update hun8.py

--- hun8.py
+++ hun8.py
@@ -2,6 +2,6 @@ n=int(input())
 m=input().split()
 for i in range(0,n):
 	for j in range(i+1,n):
-		for a in range(2,n):
-			if int(m[a])==int(m[i])+int(m[j]):
+		for a in range(j+1,n):
+			if int(m[i])+int(m[j])==int(m[a]):
 				print(int(m[i]),int(m[j]),int(m[a]))
